725406ceac1fea2c81ce42e135ef149844ee1e93
Alex Campos Arcos <alex_campos_arcos@hotmail.com>
2016-08-31 13:43:36 -0300

    Add project files.

---
+++ Crown01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Crown01.Startup))]
+namespace Crown01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
